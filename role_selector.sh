83c697b06de418fb105c7b36fdfdd1550de52ba2
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-09-17 00:09:41 -0500

    Make role selector create the 'chosen' role files.

--- role_selector.sh
+++ role_selector.sh
@@ -9,22 +9,31 @@ local -a dialogOptions;
 # Initialize file name to index to be used to manage the chosen roles.
 local -a files;
 
+#Add Height to dialog options
+dialogOptions+=(15);
+
+#Add Width to dialog options
+dialogOptions+=(50);
+
+#Add List Height to dialog options
+dialogOptions+=(10);
+
 # Initialize index (I dont think there is a for loop with iterator)
 i=1;
 for f in ~/.config/roles/options/*; do
-    # Add the file name and index for later
-    files[$i]=$f;
-
     # Add the index to show in dialog
     dialogOptions+=$i;
-    
+
     # The file contents contains the Verbage to show for the role in dialog (add it)
     contents="$(<$f)";
     dialogOptions+=("$contents");
 
     # Do string replacement for options to choosen (getting the path to check if the file has been choosen)
     chosenFile="${f//options/chosen}";
-    
+
+    # Add the file name and index for later
+    files[$i]=$chosenFile;
+
     # Check if the role is already activated and start it that way in dialog
     currentStatus="off";
     if [[ -f "$chosenFile" ]]; then
@@ -37,14 +46,13 @@ for f in ~/.config/roles/options/*; do
     i=$(( i + 1 ));
 done
 
-width=50;
-height=15;
-list_height=10;
+out="$(dialog --clear --backtitle "Menu Test" --title "This is a test for Menu entry" --checklist "Menu" ${dialogOptions} --stdout)";
+clear;
 
-arr="$(dialog --clear --backtitle "Menu Test" --title "This is a test for Menu entry" --checklist "Menu" $height $width $list_height ${dialogOptions} --output-fd 1)";
+rm ~/.config/roles/chosen/*;
 
-for i in $arr; do
-    (( j = (i * 3) + 1 ));
-    echo ${arr[$j]};
-    echo ${files[$i]};
+#            v   Split string by " " character (indicated by the v earlier) to an array.
+parts=(${(@s: :)out});#TODO: Pull this into a function so it can be reused.
+for i in ${parts[@]}; do
+    touch $files[$i];
 done
