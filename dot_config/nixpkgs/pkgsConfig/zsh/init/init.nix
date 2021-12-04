c89661017da9cc5a5c9ea0b7f7f098a64a35d0fb
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-04 10:58:38 -0600

    Remove old trace that is no longer needed

--- dot_config/nixpkgs/pkgsConfig/zsh/init/init.nix
+++ dot_config/nixpkgs/pkgsConfig/zsh/init/init.nix
@@ -7,7 +7,7 @@ in
         # Load in all the zsh files in the initExtra, initExtraBeforeCompInit, and initExtraFirst directories into the config. 
         # This allows for adding stuff to the .zshrc file (in the specified spots) from normal zsh files (where a editor will have zsh syntax highlighting).
         # This also allows breaking the content being added up into several smaller files.
-        initExtra = builtins.trace "hit" fileHelpers.combileFiles (fileHelpers.validZshFiles ./initExtra);
+        initExtra = fileHelpers.combileFiles (fileHelpers.validZshFiles ./initExtra);
         initExtraBeforeCompInit = fileHelpers.combileFiles (fileHelpers.validZshFiles ./initExtraBeforeCompInit);
         initExtraFirst = fileHelpers.combileFiles (fileHelpers.validZshFiles ./initExtraFirst);
     };
