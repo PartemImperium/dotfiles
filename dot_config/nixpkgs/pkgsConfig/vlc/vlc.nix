353bea0729cd96c14c978700123c612fd6a118df
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-05 22:46:15 -0600

    Fix bad check for isEnabled on vlc

--- dot_config/nixpkgs/pkgsConfig/vlc/vlc.nix
+++ dot_config/nixpkgs/pkgsConfig/vlc/vlc.nix
@@ -2,7 +2,7 @@
 let 
     vars = import ../../variables.nix;
     #TODO: remove this hard false and figure out why it isnt building for me
-    isEnabled = false && vars.shouldInstallApps;# This is just a direct assingment here but others may have more logic and keeping things standardized makes it easier to work with.
+    isEnabled = false && vars.shouldInstallApps && vars.isGui;# This is just a direct assingment here but others may have more logic and keeping things standardized makes it easier to work with.
 in
 {# audio and video player 
     home.packages = lib.lists.optionals isEnabled [ pkgs.vlc ];
