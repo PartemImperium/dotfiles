8ec777b9d95f65e526f679317c9f42e233fbd6d0
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-05 21:32:27 -0600

    Standardize nix-index module

--- dot_config/nixpkgs/pkgsConfig/nix-index/nix-index.nix
+++ dot_config/nixpkgs/pkgsConfig/nix-index/nix-index.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let 
+    vars = import ../../variables.nix;
+    isEnabled = true;# This is just a direct assingment here but others may have more logic and keeping things standardized makes it easier to work with.
+in
 {# index of files in installed through nix as well as a command not found/ suggestted install 
     programs.nix-index = {
-      enable = true;
+      enable = isEnabled;
       enableZshIntegration = true;
     };
-    home.packages = [ pkgs.nix-index ];
+    home.packages = lib.lists.optionals isEnabled [ pkgs.nix-index ];
 }
     
