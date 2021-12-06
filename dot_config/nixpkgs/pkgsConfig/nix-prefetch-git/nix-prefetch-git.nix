a31cf2167a8511bdffce0d420ba89691af52e297
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-05 20:25:25 -0600

    Add missing lib to newly modulized pkgConfigs....

--- dot_config/nixpkgs/pkgsConfig/nix-prefetch-git/nix-prefetch-git.nix
+++ dot_config/nixpkgs/pkgsConfig/nix-prefetch-git/nix-prefetch-git.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let 
     vars = import ../../variables.nix;
     isEnabled = true;# This is just a direct assingment here but others may have more logic and keeping things standardized makes it easier to work with.
