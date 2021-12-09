00a2218f762a61c6bd6dfbc8d9db2c97f95df253
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-08 23:38:48 -0600

    Add vscode config to module

--- dot_config/nixpkgs/pkgsConfig/vscode/vscode.nix
+++ dot_config/nixpkgs/pkgsConfig/vscode/vscode.nix
@@ -8,8 +8,23 @@ in
         enable = mkEnableOption "vscode";
     };
     config = mkIf cfg.enable {
-        programs.vscode = {#TODO: manage settings for vsc
+        programs.vscode = {
             enable = true;
+
+            userSettings = {
+                "editor.renderWhitespace" = "all";
+                "editor.bracketPairColorization.enabled" = true;
+
+                # Minimap Scrollbar settings
+                "editor.minimap.enabled" = true;
+                "editor.minimap.maxColumn" = 60;
+                "editor.minimap.showSlider" = "always";
+
+                # Terminal settings
+                "terminal.external.osxExec" = "${pkgs.alacritty}/Applications/Alacritty.app";
+                "terminal.external.linuxExec" = "${pkgs.alacritty}/bin/alacritty";
+                "terminal.integrated.fontFamily" = "Hack Nerd Font Mono";
+            };
         };
     };
 }
