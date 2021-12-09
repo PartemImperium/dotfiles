7a0f9e3884080243e445f8956fd2dfdeba572c09
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-08 22:33:27 -0600

    Remove un-neccicary addition home.packages for vscode

--- dot_config/nixpkgs/pkgsConfig/vscode/vscode.nix
+++ dot_config/nixpkgs/pkgsConfig/vscode/vscode.nix
@@ -8,7 +8,6 @@ in
         enable = mkEnableOption "vscode";
     };
     config = mkIf cfg.enable {
-        home.packages = [ pkgs.vscode ]; #TODO: I dont think this is needed
         programs.vscode = {#TODO: manage settings for vsc
             enable = true;
         };
