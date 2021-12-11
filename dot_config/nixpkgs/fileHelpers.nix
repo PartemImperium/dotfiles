71dc2ccdd0b7af909e77e61f566591eee5bdc8bc
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-11 13:05:06 -0600

    Make file helpers rec so I can have all the functions exposed

--- dot_config/nixpkgs/fileHelpers.nix
+++ dot_config/nixpkgs/fileHelpers.nix
@@ -1,8 +1,8 @@
 { lib, ... }:
 # These functions are based on this example https://github.com/evanjs/nixos_cfg/blob/4bb5b0b84a221b25cf50853c12b9f66f0cad3ea4/config/new-modules/default.nix
 # they are used to (recursivly) import all the nix files in a specified directory.
-let
-  # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
+rec {#TODO: Firure out how a lambda param to a function would work to de-dupe these functions...
+      # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
   getDir = dir: with lib; mapAttrs
     (file: type:
       if type == "directory" then 
@@ -14,9 +14,7 @@ let
 
   # Collects all files of a directory as a list of strings of paths
   files = dir: with lib; collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
-
-in 
-{#TODO: Firure out how a lambda param to a function would work to de-dupe these functions...
+    
     # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
     validNixFiles = dir: with lib; map
     (file: dir + "/${file}")
