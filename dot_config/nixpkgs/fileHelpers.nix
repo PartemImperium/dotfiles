2e23a3f6cbc13f6d8db3e31a6a67164eb09c0651
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-11 15:15:12 -0600

    De-dupe fileHelpers functions

--- dot_config/nixpkgs/fileHelpers.nix
+++ dot_config/nixpkgs/fileHelpers.nix
@@ -1,9 +1,10 @@
 { lib, ... }:
 # These functions are based on this example https://github.com/evanjs/nixos_cfg/blob/4bb5b0b84a221b25cf50853c12b9f66f0cad3ea4/config/new-modules/default.nix
 # they are used to (recursivly) import all the nix files in a specified directory.
-rec {#TODO: Firure out how a lambda param to a function would work to de-dupe these functions...
-      # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
-    getDir = dir: with lib; mapAttrs
+rec {
+    # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
+    getDir = dir: 
+      lib.mapAttrs
       (file: type:
         if type == "directory" then 
           getDir "${dir}/${file}" 
@@ -13,34 +14,28 @@ rec {#TODO: Firure out how a lambda param to a function would work to de-dupe th
       (builtins.readDir dir);
 
     # Collects all files of a directory as a list of strings of paths
-    files = dir: with lib; collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
-    
+    files = dir: lib.collect lib.isString (lib.mapAttrsRecursive (path: type: lib.concatStringsSep "/" path) (getDir dir));
+
+    # Collect all files of a directory as a list of strings of paths and filter them with the passed filter.
+    validFiles = dir: filter: map (file: dir + "/${file}") (lib.filter (filter) (files dir));
+
     # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
-    validNixFiles = dir: with lib; map
-    (file: dir + "/${file}")
-    (filter
-      (f: 
-        (hasSuffix ".nix" f) 
+    validNixFiles = dir: 
+    validFiles 
+    dir 
+    (f: 
+        (lib.hasSuffix ".nix" f) 
         && (f != "default.nix") 
         && (!lib.hasSuffix "-hm.nix" f)
-      )
-      (files dir)
     );
 
-    # Filters out directories that don't end with .zsh, also makes the strings absolute      
-    validZshFiles = dir: with lib; map
-    (file: dir + "/${file}")
-    (filter
-      (f: 
-        (hasSuffix ".zsh" f) 
-      )
-      (files dir)
-    );
+    # Filters out directories that don't end with .zsh, also makes the strings absolute
+    validZshFiles = dir: validFiles dir (f: (lib.hasSuffix ".zsh" f));
 
-    combineFiles = files: with lib;
+    #Combines the passed files into a single string
+    combineFiles = files:
       if builtins.isList files then
         builtins.concatStringsSep "" (map (f: lib.fileContents f) files)
       else
         "";
-      
 }
