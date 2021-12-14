2a93bf314aa78f3c00d28b0007ac13d2906a92e4
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-12-13 19:00:34 -0600

    Initial Inkscape application bundle overlay

---
+++ dot_config/nixpkgs/overlays/inkscape-fix.nix
@@ -0,0 +1,18 @@
+self: super: {
+    inkscape = super.inkscape.overrideAttrs (oldAttrs: rec {
+        #buildInputs = oldAttrs.buildInputs ++ [super.gspell];
+
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.makeWrapper ];
+
+        postInstall = super.lib.optionalString super.stdenv.isDarwin oldAttrs.postInstall + ''
+        mkdir -p $out/Applications/Inkscape.app/Contents/MacOS
+        makeWrapper $out/bin/inkscape $out/Applications/Inkscape.app/Contents/MacOS/inkscape
+
+        mkdir -p $out/Applications/Inkscape.app/Contents/Resources
+
+        ls
+
+        #cp packaging/macos/inkscape.plist $out/Applications/Inkscape.app/Contents/Info.plist
+        '';
+    });
+}
