3a9094271c35e0bf8c4246c73f574a97a65b0314
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2022-01-02 00:13:03 -0600

    Add a bunch of TODO's to home.nix

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -1,5 +1,5 @@
-    { config, lib, pkgs, ... }:
-    {
+{ config, lib, pkgs, ... }:
+{
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
@@ -18,9 +18,24 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    home.stateVersion = "21.05";
-    
+    home.stateVersion = "22.05";
+
     fonts.fontconfig.enable = true;
 
     nixpkgs.config.allowUnfree = true;
+    #TODO: Add preview zsh function qlmanage -p update-nix-vsc-ext.sh &> /dev/null  
+    #TODO: Add Plex app..... (curently running out of downloads folder)
+    #TODO: Look into installing this (and its prereqs) https://github.com/dado3212/spaces-renamer
+    #TODO: make https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/misc/vscode-extensions/update_installed_exts.sh nix package (instead of a random script in nipkgs repo)
+    #TODO: Look into using x86 as a fallback for packages that are not happy with m1
+    #          https://github.com/nix-dot-dev/nix.dev/issues/178
+    #          https://github.com/evanrelf/dotfiles/blob/main/modules/machines/ultraviolet.nix#L43-L52
+    #TODO: Maybe add Pro presenter (it asking for updates would get old quick though....) - https://renewedvision.com/downloads/propresenter/mac/ProPresenter_7.8_117964824.zip
+    #TODO: add ksnip a cross platform screenshot app (it currently doesnt work with darwin but I could try to modify it to make it) Looks like it has compiler-rt issue...
+    #TODO: install TopNotch from nix (it will definitly be a bin download as it isnt open source) and uninstall the non nix install
+    #TODO: Make chrome work from nix (instead of current manual install) and set up home-manager settings with it
+    #TODO: Make nix package for Magnet (prob find a new window manager as Magnet is not FOSS and is from App Store) - https://github.com/rxhanson/Rectangle this might work good (isnt a nix package yet so I would have to make it)  / look into  https://cbrgm.net/post/2021-05-5-setup-macos/ ("i3" in macos)
+    #TODO: Figure out ssh config (~/.ssh/config) (mainly for git ssh keys) 
+    #TODO: Add https://github.com/samuelmeuli/glance (likely first bin download)
+    #TODO: Manage ~/.nuget/NuGet/NuGet.Config
 }
