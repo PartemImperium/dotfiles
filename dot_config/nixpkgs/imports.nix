ca924433769cc6882a923a2d9572c840beafec49
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2022-01-25 00:01:34 -0600

    Add missed import for activation scripts

--- dot_config/nixpkgs/imports.nix
+++ dot_config/nixpkgs/imports.nix
@@ -13,6 +13,7 @@ in
      ++ optionals vars.isLinux (fileHelpers.validNixFiles ./linux)
      ++ optionals vars.isWsl (fileHelpers.validNixFiles ./wsl)
      ++ fileHelpers.validNixFiles ./pkgsConfig
+     ++ fileHelpers.validNixFiles ./activation-scripts
      # The work modules are not tracked in git. They are added manually on my work computer for anything that is work specific (and I dont want in source control). 
      # They are also added after everything else so that they can override settings set in other configs.
      ++ optionals vars.isWork (fileHelpers.validNixFiles ./work); 
